16b3332573b8530c6d008314ad709ed503ccd5cc
yutaya <yama30@DESKTOP-A5EA9RD.localdomain>
2020-05-05 18:50:49 +0900

    Q1～Q8再提出

--- task.rb
+++ task.rb
@@ -68,7 +68,7 @@ def q8
 
   # 以下に回答を記載
   programming_languages.map!(&:capitalize)
-  upper_case_programming_languages = programming_languages.map{|a|a.upcase}
+  upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
